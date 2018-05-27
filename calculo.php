b80b8e0aafd8a2c4725cfb771af672420c17312a
yassirprograma <dj.jicara@gmail.com>
2018-05-27 14:28:29 -0500

    Esta es la plantilla de nuestra paǵina

---
+++ calculo.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width", user-scalable=no initial-scale=1.0 , maximum-scale=1.0 , minimum-scale=1.0 >
+
+<title>Cálculo</title>
+
+<link rel="icon" href="img/hola.ico">
+
+<link rel="stylesheet" href="css/index_style.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+  <?php
+    include_once("cargasecciones.php"); /*  CARGA LA PLANTILLA DE TODAS LAS DEMÁS SECCIONS */
+   ?>
+
+   <div class="princisecc">
+     <article class="articulo">
+          <p> <h1>
+              <img src="img/Alge.png" width="150px"  alt=""/>
+
+          </h1> </p>
+
+
+     </article>
+     <article class="final" >
+       <?php
+          include_once("hastabajo.html");
+        ?>
+
+            <!--NO TOCAR NADA AQUÍ-->
+
+
+
+     </article>
+
+
+   </div>
+
+
+
+
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js" ></script>
+
+</body>
+
+</html>
---
+++ pruebas.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hola</title>
+    <link rel="icon" href="favico.ico">
+  </head>
+  <body>
+
+  </body>
+</html>
