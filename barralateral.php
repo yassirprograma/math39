c92249e45bc80d50e970cf62ab23c7649b987ed2
yassirprograma <dj.jicara@gmail.com>
2018-06-11 10:33:16 -0500

    Agregue bootstrap-social,fontawesome css,js(CDN), agregué enlace a github en la barra lateral y a el pie de pagina e iconos bonitos :v

--- barralateral.php
+++ barralateral.php
@@ -1,8 +1,7 @@
 
 <div  id="lateral" class="animated infinite pulse" >
 
-      <br>
-      <br>
+      
 
 
       <li>
@@ -10,7 +9,12 @@
         <br>    <!--MANDADO A LLAMAR EN CARGASECCIONES-->
         <br>
 
-        <a href="juego.php" target="_blank"> <p> ¡Juega a particles! </p>    </a>
+        <a href="juego.php" target="_blank"> <p><i class="fas fa-gamepad fa-2x"></i> ¡Juega a particles! </p>     </a>
+
+        <br>
+        <a href="https://github.com/yassirprograma/math39" target="_blank">  <center style="color:rgb(205, 205, 205);"> <span class="fab fa-github fa-4x "> </span> <br> <br> GitHub  </center> </a>
+
+
       </li>
 
 </div>
--- head.php
+++ head.php
@@ -6,3 +6,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/fontawesome-all.css">
+<link rel="stylesheet" href="css/docs.css">
+<link rel="stylesheet" href="css/bootstrap-social.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
